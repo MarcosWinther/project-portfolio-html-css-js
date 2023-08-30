a65557054c8f139f9a9011e6e5c430dcd273c077
MarcosWinther <winthermarcos@gmail.com>
2023-08-30 11:19:55 -0300

    Dinamizando as hard skills

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,8 +27,15 @@ function updateProfileSoftSkills(profileData) {
    softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('');
 }
 
+function updateProfileHardSkills(profileData) {
+   const hardSkills = document.getElementById('profile.skills.hardSkills');
+
+   hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('');
+}
+
 (async () => {
    const profileData = await fetchProfileData();
    updateProfileInfo(profileData);
    updateProfileSoftSkills(profileData);
+   updateProfileHardSkills(profileData);
 })()
